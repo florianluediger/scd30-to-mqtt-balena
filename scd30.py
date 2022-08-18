78e4860a1ede90afbff38d69dc1ea19cb24b7dbb
Florian Lüdiger <florian.luediger@googlemail.com>
2022-08-18 12:03:46 +0200

    Replace postgres upload with mqtt publish

--- scd30.py
+++ scd30.py
@@ -1,21 +1,12 @@
 import time
 from scd30_i2c import SCD30
-import psycopg2
 from datetime import datetime
+import paho.mqtt.publish as publish
+import os
 
 # configuration
 measurement_interval = 60
-database_upload_interval = 300
-
-# set up database connection
-db = psycopg2.connect(
-    host="ip",
-    database="sensordata",
-    user="postgres",
-    password="secret"
-)
-
-db_cursor = db.cursor()
+upload_interval = 300
 
 # set up SCD30 sensor
 scd30 = SCD30()
@@ -25,14 +16,19 @@ scd30.start_periodic_measurement()
 
 time.sleep(2)
 
+mqtt_server = os.environ.get('MQTT_SERVER')
+
 while 1:
     if scd30.get_data_ready():
         m = scd30.read_measurement()
         if m is not None:
-            db_cursor.execute(f"insert into co2sensor (timestamp, co2, temperatur, humidity) values ('{datetime.now()}', {m[0]:.2f}, {m[1]:.2f}, {m[2]:.2f});")
-            db.commit()
-        time.sleep(database_upload_interval)
+            publish.single("scd30/raspberry",
+                           "{" + "'time':'{}','co2':'{}','temperature':'{}','humidity':'{}'".format(datetime.now(), m[0],
+                                                                                                m[1], m[2]) + "}",
+                           hostname=mqtt_server,
+                           port=8883,
+                           tls={'ca_certs': "AmazonRootCA1.pem", 'certfile': "certificate.pem.crt",
+                                'keyfile': "private.pem.key"})
+        time.sleep(upload_interval)
     else:
         time.sleep(1)
-
-scd30.stop_periodic_measurement()
